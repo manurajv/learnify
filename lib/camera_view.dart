37c3fcb3904c4e7a3622550b489e8dd86860cdc0
manurajv <manurajv@gmail.com>
2023-08-05 12:15:19 +0530

    Manu

--- lib/camera_view.dart
+++ lib/camera_view.dart
@@ -8,11 +8,12 @@ class CameraView extends StatefulWidget {
 
 class _CameraViewState extends State<CameraView> {
   late Future<CameraController> _cameraControllerFuture;
+  late CameraDescription _currentCamera;
 
   @override
   void initState() {
     super.initState();
-    _cameraControllerFuture = _initializeCamera();
+    _initializeCamera();
   }
 
   @override
@@ -23,33 +24,63 @@ class _CameraViewState extends State<CameraView> {
     super.dispose();
   }
 
-  Future<CameraController> _initializeCamera() async {
+  Future<void> _initializeCamera() async {
     final cameras = await availableCameras();
+    _currentCamera = cameras[0]; // Set the initial camera to the back camera
     final cameraController =
-    CameraController(cameras[0], ResolutionPreset.medium);
+    CameraController(_currentCamera, ResolutionPreset.medium);
     await cameraController.initialize();
-    return cameraController;
+    setState(() {
+      _cameraControllerFuture = Future.value(cameraController);
+    });
+  }
+
+  Future<void> _switchCamera() async {
+    final cameras = await availableCameras();
+    // Toggle the active camera
+    _currentCamera = _currentCamera == cameras[0] ? cameras[1] : cameras[0];
+    final cameraController =
+    CameraController(_currentCamera, ResolutionPreset.medium);
+    await cameraController.initialize();
+    setState(() {
+      _cameraControllerFuture = Future.value(cameraController);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<CameraController>(
-      future: _cameraControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          if (snapshot.hasError) {
+    return Scaffold(
+      body: FutureBuilder<CameraController>(
+        future: _cameraControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+            final cameraController = snapshot.data!;
+            return Stack(
+              children: [
+                CameraPreview(cameraController),
+                Positioned(
+                  top: 20,
+                  right: 20,
+                  child: IconButton(
+                    icon: Icon(Icons.switch_camera),
+                    onPressed: _switchCamera,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            );
+          } else {
             return Center(
-              child: Text('Error: ${snapshot.error}'),
+              child: CircularProgressIndicator(),
             );
           }
-          final cameraController = snapshot.data!;
-          return CameraPreview(cameraController);
-        } else {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
+        },
+      ),
     );
   }
 }
