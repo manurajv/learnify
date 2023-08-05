effd8c28c72b57c436c3936d957bd2ab6bb7cf56
manurajv <manurajv@gmail.com>
2023-08-05 12:50:09 +0530

    Manu

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:learnify/face_recognition_screen.dart';
 import 'package:learnify/home_screen.dart';
+import 'package:learnify/splash_screen.dart';
 //import 'package:firebase_core/firebase_core.dart';
 
 void main() {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: HomeScreen(),
+        home: SplashScreen(),
       );
     }
     );
